5c45a974382278730240840d59a06845c3322a37
Viktor <v.mun@yahoo.com>
2019-04-07 21:49:57 +0200

    added dep and employee

---
+++ HrSystem/HrSystem/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace HrSystem
+{
+     class Department
+    {
+        public string Name { get; set; }
+        public Department(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ HrSystem/HrSystem/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace HrSystem
+{
+     class Employee
+    {
+       
+            public string Name { get; set; }
+            public string LastName { get; set; }
+            public Department Department { get; set; }
+
+            public Employee(string name, string lastName, Department department)
+            {
+                Name = name;
+                LastName = lastName;
+                Department = department;
+            }
+
+            public override string ToString()
+            {
+                return $@"{Name} {LastName} {Department}";
+            }
+       
+    }
+}
